0a8a32cd22a8141f0ef1779f61c7528033567ea8
BeemoLin <dteout@gamil.com>
2014-08-30 01:40:51 +0800

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  RAILSGUYS_DATABASE_PASSWORD = 'bemore454053'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
