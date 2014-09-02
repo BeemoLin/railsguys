5256ae1bda9ad102f126e66ad744d77e3ee1b390
BeemoLin <dteout@gamil.com>
2014-09-02 17:36:32 +0800

    html modify

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def comment_user(id)
+   user = User.select(:email).where(User.arel_table[:id].eq(id)).order(:created_at).reverse_order
+   
+   if user.empty?
+     email||= ''
+   else
+     email||= user[0].email
+   end
+   
+   return email
+  end
 end
