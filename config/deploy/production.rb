6be6b2e6ba3909db78f8af4f9f5368457805e4ad
BeemoLin <dteout@gamil.com>
2014-09-12 15:14:42 +0800

    www

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@www.cutego.tw}
-role :web, %w{deploy@www.cutego.tw}
-role :db,  %w{deploy@www.cutego.tw}
+role :app, %w{deploy@cutego.tw}
+role :web, %w{deploy@cutego.tw}
+role :db,  %w{deploy@cutego.tw}
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +15,7 @@ role :db,  %w{deploy@www.cutego.tw}
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 set :stage, :production
-server 'www.cutego.tw', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'cutego.tw', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
